a2ccea15c7509a7ee5fab499e344f2159e8fac98
Cheithanya PR <cheithanya2002@gmail.com>
2022-08-31 12:16:13 +0530

    'Initial-project-created'

---
+++ config.py
@@ -0,0 +1,19 @@
+##OPEN API STUFF
+OPENAI_API_KEY = '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'
+
+
+
+## FLASK STUFF
+class Config(object):
+    DEBUG = True
+    TESTING = False
+
+class DevelopmentConfig(Config):
+    SECRET_KEY = "this-is-a-super-secret-key"
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': DevelopmentConfig,
+    'production': DevelopmentConfig
+}
